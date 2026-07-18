e6d2f74ebd042c31bd6e62f9842df2921fd3bc1f
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-18 14:01:31 -0400

    Create R_PlotSquared_6_7.java

---
+++ src/main/java/dev/tehbrian/mayi/paper/restrictions/R_PlotSquared_6_7.java
@@ -0,0 +1,11 @@
+package dev.tehbrian.mayi.paper.restrictions;
+
+import dev.tehbrian.mayi.paper.PaperRestrictionLoader.RestrictionAdapter;
+
+public final class R_PlotSquared_6_7 implements RestrictionAdapter {
+
+	@Override
+	public String pluginName() {
+		return "PlotSquared";
+	}
+}
